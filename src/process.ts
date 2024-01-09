d69641f609d7af94f5af558906c0dc6376cebca9
heppaat <pali.herrer@gmail.com>
2024-01-09 16:39:07 +0100

    zod and ts set-up

---
+++ src/process.ts
@@ -0,0 +1,17 @@
+//import * as fs from 'node:fs';
+import fs from "fs/promises";
+import { z } from "zod";
+
+const movieDB = {
+  professionals: [],
+  movies: [],
+  genres: [],
+};
+
+//write you code after this line
+
+console.log("Hello world");
+
+//write your code brefore this line
+
+export { movieDB };
