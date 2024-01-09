d9a5a7d0377cbc0cfbb89fa4e11add0e2a898e67
heppaat <pali.herrer@gmail.com>
2024-01-09 18:22:35 +0100

    zod mainSchema working good

--- src/process.ts
+++ src/process.ts
@@ -10,8 +10,48 @@ const movieDB = {
 
 //write you code after this line
 
-console.log("Hello world");
+const movieSchema = z.object({
+  movies: z
+    .object({
+      title: z.string(),
+      year: z.number(),
+      runtime: z.number().optional(),
+      genres: z.array(z.string()),
+      "release-date": z.string(),
+      writers: z.array(z.string()),
+      storyline: z.string().optional(),
+      directors: z.array(z.string()),
+    })
+    .array(),
+});
 
+type MainData = z.infer<typeof movieSchema>;
+
+const myFunction = (data: MainData) => {
+  const myData = data.movies;
+  for (let i = 0; i < myData.length; i++) {
+    const element = myData[i];
+    console.log(element);
+  }
+};
+
+const readData = async () => {
+  try {
+    const input = await fs.readFile(`${__dirname}/../data.json`, "utf-8");
+    const jsonData = JSON.parse(input);
+
+    const result = movieSchema.safeParse(jsonData);
+
+    if (!result.success) return console.log(result.error.issues);
+
+    const validatedData = result.data;
+    myFunction(validatedData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+readData();
 //write your code brefore this line
 
 export { movieDB };
---
+++ test_data.js
@@ -0,0 +1,80 @@
+{
+  "movies": [
+    {
+      "title": "2 Fast 2 Furious",
+      "year": 2003,
+      "runtime": 103,
+      "genres": [
+        "action",
+        "crime",
+        "thriller"
+      ],
+      "release-date": "2003-06-06",
+      "writers": [
+        "Gary Scott Thompson",
+        "Michael Brandt",
+        "Derek Haas"
+      ],
+      "actors": [
+        "Paul Walker",
+        "Tyrese Gibson",
+        "Chris Ludacris Bridges",
+        "Eva Mendes",
+        "Cole Hauser"
+      ],
+      "storyline": "Former Cop Brian O'Corner is assigned to bring down drug lord Carter Verone",
+      "directors": [
+        "John Singleton"
+      ]
+    },
+    {
+      "title": "28 Days Later",
+      "year": 2002,
+      "runtime": 113,
+      "genres": [
+        "drama",
+        "horror",
+        "sci-fi"
+      ],
+      "release-date": "2002-06-27",
+      "writers": [
+        "Alex Garland"
+      ],
+      "actors": [
+        "Cillian Murphy",
+        "Naomie Harris",
+        "Christopher Eccleston"
+      ],
+      "storyline": "Four weeks after a mysterious, incurable virus spreads throughout the UK, a handful of survivors try to find sanctuary.",
+      "directors": [
+        "Danny Boyle"
+      ]
+    },
+    {
+      "title": "50 First Dates",
+      "year": 2004,
+      "runtime": 99,
+      "genres": [
+        "comedy",
+        "drama",
+        "romance"
+      ],
+      "release-date": "2004-02-13",
+      "writers": [
+        "George Wing"
+      ],
+      "actors": [
+        "Adam Sandler",
+        "Drew Barrymore",
+        "Rob Schneider",
+        "Sean Astin",
+        "Blake Clark",
+        "Dan Aykroyd"
+      ],
+      "description": "Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he's finally found the girl of his dreams, until he discovers she has short-term memory loss and forgets him the next day.",
+      "directors": [
+        "Peter Segal"
+      ]
+    }
+]
+}
